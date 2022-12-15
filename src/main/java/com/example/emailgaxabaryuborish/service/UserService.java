1787fdbe537cc4b7899abfd903289c55f9fd4364
Akmalbek2002 <axrorovakmal256@gmail.com>
2022-12-15 10:46:45 +0300

    yangilandi

--- src/main/java/com/example/emailgaxabaryuborish/service/UserService.java
+++ src/main/java/com/example/emailgaxabaryuborish/service/UserService.java
@@ -1,5 +1,6 @@
 package com.example.emailgaxabaryuborish.service;
 
+import com.example.emailgaxabaryuborish.entity.Users;
 import com.example.emailgaxabaryuborish.payload.ApiResponse;
 import com.example.emailgaxabaryuborish.payload.UserDto;
 import com.example.emailgaxabaryuborish.repository.UserRepository;
@@ -21,5 +22,11 @@ public class UserService {
         if(b){
             return new ApiResponse("Bunday foydalanuvchi mavjud",false);
         }
+        Users users=new Users();
+        users.setIsm(users.getIsm());
+        users.setFamiliya(users.getFamiliya());
+        users.setTelNomer(users.getTelNomer());
+        users.setUsername(users.getUsername());
+        users.setPassword(passwordEncoder.encode(userDto.getPassword()));
     }
 }
