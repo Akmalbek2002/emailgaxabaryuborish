11308e7181058e6740031d73a577f877bceb77ed
Akmalbek2002 <axrorovakmal256@gmail.com>
2022-12-21 10:02:58 +0300

    yangilandi

---
+++ src/main/java/com/example/emailgaxabaryuborish/entity/Mahsulot.java
@@ -0,0 +1,2 @@
+package com.example.emailgaxabaryuborish.entity;public class Mahsulot {
+}
---
+++ src/main/java/com/example/emailgaxabaryuborish/repository/MahsulotRepository.java
@@ -0,0 +1,2 @@
+package com.example.emailgaxabaryuborish.repository;public interface MahsulotRepository {
+}
